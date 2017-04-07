8319dd1299103a337ffbeb6ad18a96d799f78dee
ariverap2016 <albariverapulido@gmail.com>
2017-04-07 13:12:48 +0200

    upload with the same port

--- server.py
+++ server.py
@@ -1,17 +1,29 @@
-import web
-import socketserver #servidor enchufe
-# ping para comprobar la identidad, de da la direccion IP de lo q quieras buscar
+#<Drugs OpenFDA>
+#    Copyright (C) <2017>  <Alba>
 
-###
-#WEB server
-#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import web
+import socketserver
 
 PORT=8000
 
-#Handler = http.server.SimpleHTTPRequestHandler #es directamente una referencia a la clase
-Handler = web.testHTTPRequestHandler
+socketserver.TCPServer.allow_reuse_address = True
+#allow_reuse_address es una variable q permite reutilizar el puerto 8000
 
+Handler = web.testHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
-# httpd es un objeto q se ejecuta para siempre
+
 print("serving at port", PORT)
 httpd.serve_forever()
