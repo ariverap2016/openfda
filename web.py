18599d241f3ab01367b40730796027424794c0ac
ariverap2016 <albariverapulido@gmail.com>
2017-04-24 15:55:10 +0200

    Add files via upload

--- web.py
+++ web.py
@@ -1,7 +1,18 @@
+#<Drugs OpenFDA>
+#    Copyright (C) <2017>  <Alba>
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import http.server
 import http.client
 import json
-#import socketserver
 class OpenFDAClient():
     OPENFDA_API_URL = "api.fda.gov"
     OPENFDA_API_EVENT = "/drug/event.json"
@@ -36,28 +47,31 @@ class OpenFDAHTML():
         <html>
             <head>
                 <title>OpenFDA Cool App</title>
+                <style>
+                form {text-align: center; }
+                </style>
             </head>
             <body>
-                <h1>OpenFDA Client</h1>
+                <h1 align='center'>OpenFDA Client</h1>
                 <form method="get" action="listDrugs">
-                    Drug List:<input type="submit" value="Drug List: Send to OpenFDA">
-                    Limit:<input name="number" size='2' type="text">
+                    Drug List <BR> <input type="submit" value="Drug List: Send to OpenFDA">
+                    <BR> Limit:<input name="number" size='2' type="text">
                 </form>
                 <form method="get" action="searchDrug">
-                    Drug Search:<input name="drug" type="text">
-                    <input type="submit" value="Drug Search: Send to OpenFDA">
+                    <BR> Drug Search <BR> <input name="drug" type="text">
+                    <BR> <input type="submit" value="Drug Search: Send to OpenFDA">
                 </form>
                 <form method="get" action="listCompanies">
-                    Companies List:<input type="submit" value="Companies List: Send to OpenFDA">
-                    Limit:<input name="number" size='2' type="text">
+                    <BR> Companies List <BR> <input type="submit" value="Companies List: Send to OpenFDA">
+                    <BR> Limit:<input name="number" size='2' type="text">
                 </form>
                 <form method="get" action="searchCompany">
-                    Company Search:<input name="company" type="text">
-                    <input type="submit" value="Company Search: Send to OpenFDA">
+                    <BR> Company Search <BR> <input name="company" type="text">
+                    <BR> <input type="submit" value="Company Search: Send to OpenFDA">
                 </form>
                 <form method="get" action="listGender">
-                    Patientsex List:<input type="submit" value="Patientsex List: Send to OpenFDA">
-                    Limit:<input name="number" size='2' type="text">
+                    <BR> Patientsex List <BR> <input type="submit" value="Patientsex List: Send to OpenFDA">
+                    <BR> Limit:<input name="number" size='2' type="text">
                 </form>
             </body>
         </html>
@@ -93,6 +107,19 @@ class OpenFDAHTML():
         </html>
         '''
         return html
+    def get_html_secret(self):
+        html='''
+        <html>
+            <head>
+                <title>Unauthorized</title>
+            </head>
+            <body>
+                <h1>Error</h1>
+                <p> WWW-Authenticate de basic Realm </p>
+            </body>
+        </html>
+        '''
+        return html
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def get_html_answer(self):
         self.send_response(200)
@@ -109,6 +136,8 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         listCompanies = False
         searchCompany = False
         listGender = False
+        secret = False
+        redirect = False
         if  self.path=='/':
             main_page=True
         elif '/listDrugs' in self.path:
@@ -121,6 +150,10 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             searchCompany = True
         elif '/listGender' in self.path:
             listGender = True
+        elif '/secret' in self.path:
+            secret = True
+        elif '/redirect' in self.path:
+            redirect = True
         if main_page:
             self.get_html_answer()
             self.wfile.write(bytes(html, "utf8"))
@@ -174,6 +207,14 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             events= parser.get_patientsex(events)
             html=HTML.get_list_html(events)
             self.wfile.write(bytes(html, "utf8"))
+        elif secret:
+            self.send_response(401)
+            self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
+            self.end_headers()
+        elif redirect:
+            self.send_response(302)
+            self.send_header('Location', 'http://localhost:8000/')
+            self.end_headers()
         else:
             self.send_response(404)
             self.send_header('Content-type','text/html')
